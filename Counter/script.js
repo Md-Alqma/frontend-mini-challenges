0439ab0c31705b42f670782219e097be0c70128a
Md-Alqma <mdalqma7@gmail.com>
2023-09-21 21:19:02 +0530

    counter complete

--- Counter/script.js
+++ Counter/script.js
@@ -2,13 +2,29 @@ const showCount = document.querySelector(".count");
 const increament = document.querySelector(".increase");
 const decreament = document.querySelector(".decrease");
 const reset = document.querySelector(".reset");
+const steps = document.querySelector(".step-count");
+
+// console.log(steps.value);
+// const stepCount = steps.value || 1;
+
+// steps.addEventListener("change", () => {
+//   stepCount = steps.value;
+// });
 
 increament.addEventListener("click", () => {
-  showCount.innerHTML = Number(showCount.innerHTML) + 1;
+  showCount.innerHTML = Number(showCount.innerHTML) + steps.valueAsNumber;
 });
 
 decreament.addEventListener("click", () => {
-  showCount.innerHTML = Number(showCount.innerHTML) - 1;
+  showCount.innerHTML = Number(showCount.innerHTML) - Number(steps.value);
+});
+
+steps.addEventListener("change", () => {
+  const stepsValue = steps.valueAsNumber;
+
+  if (Number.isNaN(stepsValue) || stepsValue < 0) {
+    steps.value = 1;
+  }
 });
 
 reset.addEventListener("click", () => {
